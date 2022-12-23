840dc24fe427fe68560dea1dc8f3a6b6a6689e5b
Auggievf <auggievf@gmail.com>
2022-12-23 10:05:51 -0800

    Adding multiple games tabulation of scores for match winner

--- js/script.js
+++ js/script.js
@@ -17,30 +17,37 @@ function getPlayerChoice() {
 }
 
 function playRound(player, computer) {
+    let winner = 0;
     switch(player) {
         case 'rock':
             if (computer === 0) {
-                console.log('Rock and Rock:  Tie!')
+                console.log('Rock and Rock:  Tie!');
             } else if (computer === 1) {
-                console.log('Paper beats Rock: Computer Wins!')
+                console.log('Paper beats Rock: Computer Wins!');
+                winner--;
             } else {
                 console.log('Rock beats Scissors: You Win!');
+                winner++;
             }
             break;
         case 'paper':
             if (computer === 0) {
                 console.log('Paper beats Rock: You Win!');
+                winner++;
             } else if (computer === 1) {
-                console.log('Paper and Paper: Tie!')
+                console.log('Paper and Paper: Tie!');
             } else {
-                console.log('You Win');
+                console.log('Scissors beats Paper: Computer Wins!');
+                winner--;
             }
             break;
         case 'scissors':
             if (computer === 0) {
                 console.log('Rock beats Scissors: Computer Wins!');
+                winner--;
             } else if (computer === 1) {
-                console.log('Scissors beats Paper: You Win!')
+                console.log('Scissors beats Paper: You Win!');
+                winner++;
             } else {
                 console.log('Scissors and Scissors: Tie!');
             }
@@ -48,16 +55,26 @@ function playRound(player, computer) {
         default:
             console.log('Looks like you didnt enter a proper response. Rock : Paper: Scissors');
     }
+    return winner;
 }
 
 function game(games){
+    let pscore = 0;
     for (let i = 0; i < games; i++) {
         let player = getPlayerChoice();
         let computer = getComputerChoice();
-        //console.log(`${player}` + `${computer}`);
-        playRound(player, computer);
+        //console.log(`${player} ${computer}`);
+        pscore = pscore + playRound(player, computer);
+    }
+    
+    if (pscore > 0) {
+        console.log('You win the whole match!');
+    } else if (pscore < 0) {
+        console.log('The computer won the match');
+    } else {
+        console.log('You tied!  There is no direct winner though you could say that you won because you had this experience and the computer will never have any experiences.');
     }
 }
 
 //Play the game with the number of games to be played passed as a parameter
-game(3);
+game(2);
